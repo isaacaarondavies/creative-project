5c16a7878c93815118b6e63b3d19129ae8fbf95e
isaacaarondavies <isaac151@byu.edu>
2021-11-03 17:57:36 -0600

    new changes

--- social-vue-v2/src/main.js
+++ social-vue-v2/src/main.js
@@ -16,8 +16,11 @@
  */
 import Vue from 'vue';
 import App from './App.vue';
+
 // You can change this import to `import router from './starterRouter'` to quickly start development from a blank layout.
 import router from './starterRouter';
+
+import './registerServiceWorker'
 // import NowUiKit from './plugins/now-ui-kit.js';
 
 Vue.config.productionTip = false;
