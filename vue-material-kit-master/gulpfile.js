c0d81f33155e81896179e59bf0a29a928a23f563
isaacaarondavies <isaac151@byu.edu>
2021-12-05 21:12:36 -0700

    Update gulpfile.js

--- vue-material-kit-master/gulpfile.js
+++ vue-material-kit-master/gulpfile.js
@@ -1,11 +1,8 @@
 var gulp = require('gulp');
-var FwdRef = require('undertaker-forward-reference');
 var license = require('gulp-license');
 var pump = require('pump');
 
-gulp.registry(FwdRef()); // or gulp.registry(new FwdRef());
-
-gulp.task('license', function (cb) {
+function license(cb) {
   pump([
       gulp.src('./lib/*.js'),
       license('MIT', {tiny: true}),
