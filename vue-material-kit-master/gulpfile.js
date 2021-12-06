4c3e3bd227e3245e18779e4a8e30096ef3b5aaa7
isaacaarondavies <isaac151@byu.edu>
2021-12-05 21:01:54 -0700

    Update gulpfile.js

--- vue-material-kit-master/gulpfile.js
+++ vue-material-kit-master/gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require('gulp');
 var license = require('gulp-license');
 var pump = require('pump');
+var FwdRef = require('undertaker-forward-reference');
+
+gulp.registry(FwdRef()); // or gulp.registry(new FwdRef());
+
+gulp.task('default', gulp.series('forward-ref'));
+
+gulp.task('forward-ref', function(cb){
+  // do task things
+  cb();
+});
 
 gulp.task('license', function (cb) {
   pump([
