8d33edf189e081e226880329c09e622f4f0bd9e6
isaacaarondavies <isaac151@byu.edu>
2021-12-05 21:18:52 -0700

    Update gulpfile.js

--- vue-material-kit-master/gulpfile.js
+++ vue-material-kit-master/gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp');
-var license = require('gulp-license');
+var licenses = require('gulp-license');
 var pump = require('pump');
 
 function license(cb) {
   pump([
       gulp.src('./lib/*.js'),
-      license('MIT', {tiny: true}),
+      licenses('MIT', {tiny: true}),
       gulp.dest('./dist')
     ],
     cb
